7d49c67bf577225050b772eb060258fbbcc0eb7d
EduardoResendiz <eduardoresendiz5491@gmail.com>
2019-08-16 11:00:36 -0500

    Se sube punto 6 si checkbox ni maquetacion

--- src/app/pages/punto-seis/punto-seis.page.ts
+++ src/app/pages/punto-seis/punto-seis.page.ts
@@ -31,4 +31,27 @@ export class PuntoSeisPage implements OnInit {
 
   enviar(form) {}
 
+  enviarForm(formulario){
+    const nombre=formulario.form.value.nombre
+    const caracteristicas=formulario.form.value.caracteristicas
+    const requerimientos=formulario.form.value.requerimientos
+    const conocimientos=formulario.form.value.conocimientos
+    const tipos=formulario.form.value.tipos
+    const nivel=formulario.form.value.nivel
+       console.log(nombre,caracteristicas,requerimientos,conocimientos,tipos,nivel);
+       
+  }
+
+  enviarForm2(formulario2){
+    const objetivo=formulario2.form.value.objetivo
+    const funciones=formulario2.form.value.funciones
+    const personal=formulario2.form.value.personal
+    const herramientas=formulario2.form.value.herramientas
+    const proteccion=formulario2.form.value.proteccion
+    
+    console.log(objetivo,funciones,personal,herramientas,proteccion);
+    
+
+  }
+
 }
