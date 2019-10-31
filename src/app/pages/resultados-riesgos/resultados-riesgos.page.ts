c2764c54fb52ad917e85432642d93ebe935f4ff8
EduardoResendiz <eduardoresendiz5491@gmail.com>
2019-10-31 13:00:47 -0600

    JSons 1-3

--- src/app/pages/resultados-riesgos/resultados-riesgos.page.ts
+++ src/app/pages/resultados-riesgos/resultados-riesgos.page.ts
@@ -8,13 +8,64 @@ import { PdfMakerService } from 'src/app/services/pdf-maker.service';
 })
 export class ResultadosRiesgosPage implements OnInit {
 respuesta:any={};
+datos:any={
+  P1C1:'',
+  P2C1:'',
+  P3C1:'',
+  P4C1:'',
+  P5C1:'',
+  P6C1:'',
+  P7C1:'',
+  P1C2:'',
+  P2C2:'',
+  P3C2:'',
+  P4C2:'',
+  P5C2:'',
+  P6C2:'',
+  P7C2:'',
+  P1C3:'',
+  P2C3:'',
+  P3C3:'',
+  P4C3:'',
+  P5C3:'',
+  P6C3:'',
+  P7C3:'',
+  P1C4:'',
+  P2C4:'',
+  P3C4:'',
+  P4C4:'',
+  P5C4:'',
+  P6C4:'',
+  P7C4:'',
+  P1C5:'',
+  P2C5:'',
+  P3C5:'',
+  P4C5:'',
+  P5C5:'',
+  P6C5:'',
+  P7C5:'',
+  P1C6:'',
+  P2C6:'',
+  P3C6:'',
+  P4C6:'',
+  P5C6:'',
+  P6C6:'',
+  P7C6:'',
+  P1C7:'',
+  P2C7:'',
+  P3C7:'',
+  P4C7:'',
+  P5C7:'',
+  P6C7:'',
+  P7C7:'',
+}
   constructor(private pdfMaker: PdfMakerService) { }
 
   ngOnInit() {
   }
 
  enviarForm(formulario){
-  console.log('Enviando');
+  console.log(this.datos);
   
  }
   pdf(){
@@ -56,15 +107,15 @@ body: [
                     widths: [20,110,90,110,140,10,10,10],
                     body:[
                         [{text:'NO',bold:true,fillColor:'#ddd'},{text:'Falla',bold:true,fillColor:'#ddd'},{text:'Causas de Falla',bold:true,fillColor:'#ddd'},{text:'Consecuencias',bold:true,fillColor:'#ddd'},{text:'Medidas de Control',bold:true,fillColor:'#ddd'},{text:'F',bold:true,fillColor:'#ddd'},{text:'C',bold:true,fillColor:'#ddd'},{text:'R',bold:true,fillColor:'#ddd'},],
-                        [{text:'1'},{text:''},{text:''},{text:''},{text:''},{text:''},{text:''},{text:''},],                       
-                        [{text:'2'},{text:''},{text:''},{text:''},{text:''},{text:''},{text:''},{text:''},],                       
-                        [{text:'3'},{text:''},{text:''},{text:''},{text:''},{text:''},{text:''},{text:''},],                       
-                        [{text:'4'},{text:''},{text:''},{text:''},{text:''},{text:''},{text:''},{text:''},],                       
-                        [{text:'5'},{text:''},{text:''},{text:''},{text:''},{text:''},{text:''},{text:''},],                       
-                        [{text:'6'},{text:''},{text:''},{text:''},{text:''},{text:''},{text:''},{text:''},],                       
-                        [{text:'7 '},{text:''},{text:''},{text:''},{text:''},{text:''},{text:''},{text:''},],                       
-                        [{text:'8'},{text:''},{text:''},{text:''},{text:''},{text:''},{text:''},{text:''},],                       
-                        [{text:'9'},{text:''},{text:''},{text:''},{text:''},{text:''},{text:''},{text:''},],                       
+                        [{text:'1'},{text:`${this.datos.P1C1}`,fontSize:8},{text:`${this.datos.P2C1}`,fontSize:8},{text:`${this.datos.P3C1}`,fontSize:8},{text:`${this.datos.P4C1}`,fontSize:8},{text:`${this.datos.P5C1}`,fontSize:8},{text:`${this.datos.P6C1}`,fontSize:8},{text:`${this.datos.P7C1}`,fontSize:8}],                       
+                        [{text:'2'},{text:`${this.datos.P1C2}`,fontSize:8},{text:`${this.datos.P2C2}`,fontSize:8},{text:`${this.datos.P3C2}`,fontSize:8},{text:`${this.datos.P4C2}`,fontSize:8},{text:`${this.datos.P5C2}`,fontSize:8},{text:`${this.datos.P6C2}`,fontSize:8},{text:`${this.datos.P7C2}`,fontSize:8}],                       
+                        [{text:'3'},{text:`${this.datos.P1C3}`,fontSize:8},{text:`${this.datos.P2C3}`,fontSize:8},{text:`${this.datos.P3C3}`,fontSize:8},{text:`${this.datos.P4C3}`,fontSize:8},{text:`${this.datos.P5C3}`,fontSize:8},{text:`${this.datos.P6C3}`,fontSize:8},{text:`${this.datos.P7C3}`,fontSize:8}],                       
+                        [{text:'4'},{text:`${this.datos.P1C4}`,fontSize:8},{text:`${this.datos.P2C4}`,fontSize:8},{text:`${this.datos.P3C4}`,fontSize:8},{text:`${this.datos.P4C4}`,fontSize:8},{text:`${this.datos.P5C4}`,fontSize:8},{text:`${this.datos.P6C4}`,fontSize:8},{text:`${this.datos.P7C4}`,fontSize:8}],                       
+                        [{text:'5'},{text:`${this.datos.P1C5}`,fontSize:8},{text:`${this.datos.P2C5}`,fontSize:8},{text:`${this.datos.P3C5}`,fontSize:8},{text:`${this.datos.P4C5}`,fontSize:8},{text:`${this.datos.P5C5}`,fontSize:8},{text:`${this.datos.P6C5}`,fontSize:8},{text:`${this.datos.P7C5}`,fontSize:8}],                       
+                        [{text:'6'},{text:`${this.datos.P1C6}`,fontSize:8},{text:`${this.datos.P2C6}`,fontSize:8},{text:`${this.datos.P3C6}`,fontSize:8},{text:`${this.datos.P4C6}`,fontSize:8},{text:`${this.datos.P5C6}`,fontSize:8},{text:`${this.datos.P6C6}`,fontSize:8},{text:`${this.datos.P7C6}`,fontSize:8}],                       
+                        [{text:'7'},{text:`${this.datos.P1C7}`,fontSize:8},{text:`${this.datos.P2C7}`,fontSize:8},{text:`${this.datos.P3C7}`,fontSize:8},{text:`${this.datos.P4C7}`,fontSize:8},{text:`${this.datos.P5C7}`,fontSize:8},{text:`${this.datos.P6C7}`,fontSize:8},{text:`${this.datos.P7C7}`,fontSize:8}],                       
+                        [{text:'8'},{text:`${this.datos.P1C8}`,fontSize:8},{text:`${this.datos.P2C8}`,fontSize:8},{text:`${this.datos.P3C8}`,fontSize:8},{text:`${this.datos.P4C8}`,fontSize:8},{text:`${this.datos.P5C8}`,fontSize:8},{text:`${this.datos.P6C8}`,fontSize:8},{text:`${this.datos.P7C8}`,fontSize:8}],                       
+                        [{text:'9'},{text:`${this.datos.P1C9}`,fontSize:8},{text:`${this.datos.P2C9}`,fontSize:8},{text:`${this.datos.P3C9}`,fontSize:8},{text:`${this.datos.P4C9}`,fontSize:8},{text:`${this.datos.P5C9}`,fontSize:8},{text:`${this.datos.P6C9}`,fontSize:8},{text:`${this.datos.P7C9}`,fontSize:8}],                       
                         [{text:'10'},{text:''},{text:''},{text:''},{text:''},{text:''},{text:''},{text:''},],                       
                         [{text:'11'},{text:''},{text:''},{text:''},{text:''},{text:''},{text:''},{text:''},],                       
                         [{text:'12'},{text:''},{text:''},{text:''},{text:''},{text:''},{text:''},{text:''},],                       
