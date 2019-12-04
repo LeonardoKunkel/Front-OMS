7fd42747064880ef7046fdb0a6640e7eefbb2230
Alejandro <alexsotog82@gmail.com>
2019-12-04 10:52:49 -0600

    camera-component

--- src/app/pages/punto-uno-evidencia/punto-uno-evidencia.page.ts
+++ src/app/pages/punto-uno-evidencia/punto-uno-evidencia.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Camera, CameraOptions } from '@ionic-native/camera/ngx';
 
 @Component({
   selector: 'app-punto-uno-evidencia',
@@ -7,9 +8,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PuntoUnoEvidenciaPage implements OnInit {
 
-  constructor() { }
+  foto: any;
+
+  constructor(private camera: Camera) { }
 
   ngOnInit() {
+  }
+
+  capturarFoto() {
+    this.camera.getPicture({
+      quality:100,
+      destinationType: this.camera.DestinationType.DATA_URL,
+      encodingType: this.camera.EncodingType.JPEG,
+      mediaType: this.camera.MediaType.PICTURE,
+      allowEdit: false,
+      saveToPhotoAlbum: true,
+      sourceType: this.camera.PictureSourceType.CAMERA
+    }).then(imagenData => this.foto = 'data:image/jpeg;base64' + imagenData );
+  }
+
+  buscarFoto() {
+    this.camera.getPicture({
+      quality:100,
+      destinationType: this.camera.DestinationType.DATA_URL,
+      encodingType: this.camera.EncodingType.JPEG,
+      mediaType: this.camera.MediaType.PICTURE,
+      allowEdit: false,
+      saveToPhotoAlbum: true,
+      sourceType: this.camera.PictureSourceType.PHOTOLIBRARY
+    }).then(imagenData => this.foto = 'data:image/jpeg;base64' + imagenData );
   }
 
 }
