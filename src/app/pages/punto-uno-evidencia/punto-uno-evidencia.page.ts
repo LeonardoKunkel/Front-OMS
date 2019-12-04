45720217e04e9f7a2b51a231b8d9c8e838bfa95b
Alejandro <alexsotog82@gmail.com>
2019-12-04 12:25:50 -0600

    se mejora evidencia

--- src/app/pages/punto-uno-evidencia/punto-uno-evidencia.page.ts
+++ src/app/pages/punto-uno-evidencia/punto-uno-evidencia.page.ts
@@ -1,3 +1,7 @@
+import { Platform } from '@ionic/angular';
+import { FileTransfer } from '@ionic-native/file-transfer/ngx';
+import { File } from '@ionic-native/file/ngx';
+import { DocumentViewer, DocumentViewerOptions } from '@ionic-native/document-viewer/ngx';
 import { Component, OnInit } from '@angular/core';
 import { Camera, CameraOptions } from '@ionic-native/camera/ngx';
 
@@ -10,9 +14,32 @@ export class PuntoUnoEvidenciaPage implements OnInit {
 
   foto: any;
 
-  constructor(private camera: Camera) { }
+  constructor(private camera: Camera, private platform: Platform, private document: DocumentViewer, private file: File, private transfer: FileTransfer) { }
 
   ngOnInit() {
+  }
+
+  abrirArchivos() {
+    const options: DocumentViewerOptions = {
+      title:'Evidencia'
+    };
+    this.document.viewDocument('assets/CARTA RESPONSIVA CONTRATISTAS.pdf', 'aplication/pdf', options);
+  }
+
+  bajarArchivo() {
+    let path = null;
+
+    if (this.platform.is('ios')) {
+      path = this.file.documentsDirectory;
+    } else {
+      path = this.file.dataDirectory;
+    }
+
+    const transfer = this.transfer.create();
+    transfer.download('ruta', path + 'myfile.pdf').then(entry => {
+      let url = entry.toUrl();
+      this.document.viewDocument(url, 'application/pdf', {});
+    });
   }
 
   capturarFoto() {
@@ -38,5 +65,7 @@ export class PuntoUnoEvidenciaPage implements OnInit {
       sourceType: this.camera.PictureSourceType.PHOTOLIBRARY
     }).then(imagenData => this.foto = 'data:image/jpeg;base64' + imagenData );
   }
+
+  subir() {}
 
 }
