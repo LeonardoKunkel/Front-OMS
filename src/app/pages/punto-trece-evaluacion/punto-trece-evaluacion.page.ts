fc691a41b5b978739f11e16ab08361b7670daa27
unknown <mr_kunkel@hotmail.com>
2020-01-23 18:11:41 -0600

    subo avance 23012020

--- src/app/pages/punto-trece-evaluacion/punto-trece-evaluacion.page.ts
+++ src/app/pages/punto-trece-evaluacion/punto-trece-evaluacion.page.ts
@@ -7,6 +7,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PuntoTreceEvaluacionPage implements OnInit {
 
+  datos:any = {
+    fecha1:'',
+    texto1:'',
+    texto2:'',
+    texto3:'',
+    texto4:'',
+    texto5:'',
+    texto6:'',
+    texto7:'',
+    texto8:'',
+    texto9:'',
+    texto10:'',
+    texto11:'',
+    texto12:'',
+    texto13:'',
+    texto14:'',
+  }
+
   constructor() { }
 
   ngOnInit() {
