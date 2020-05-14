b55553ec9cc028138f13ccb97b927ddcda0e1d3e
Eduardo <eduardoresendiz5491@gmail.com>
2020-05-14 09:05:34 -0500

    Elemento 4 dejas las condiciones

--- src/app/pages/punto-cinco-evidencia/punto-cinco-evidencia.page.ts
+++ src/app/pages/punto-cinco-evidencia/punto-cinco-evidencia.page.ts
@@ -1,9 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { Platform } from '@ionic/angular';
 import { FileTransfer } from '@ionic-native/file-transfer/ngx';
-import { File } from '@ionic-native/file/ngx';
+//import { File } from '@ionic-native/file/ngx';
 import { DocumentViewer, DocumentViewerOptions } from '@ionic-native/document-viewer/ngx';
 import { Camera, CameraOptions } from '@ionic-native/camera/ngx';
+import { EvidenciaElementoCincoService } from '../../services/Elemento 5/evidencia-elemento-cinco.service';
 
 @Component({
   selector: 'app-punto-cinco-evidencia',
@@ -12,11 +13,34 @@ import { Camera, CameraOptions } from '@ionic-native/camera/ngx';
 })
 export class PuntoCincoEvidenciaPage implements OnInit {
 
+  photoSelected: string | ArrayBuffer;
+  
+  file:File;
+
   foto: any ;
 
-  constructor(private camera: Camera, private platform: Platform, private document: DocumentViewer, private file: File, private transfer: FileTransfer) { }
+  constructor(private camera: Camera, private platform: Platform, 
+    private document: DocumentViewer, 
+    //private file: File, 
+    private transfer: FileTransfer,
+    private _uploadService:EvidenciaElementoCincoService
+    ) { }
 
   ngOnInit() {
+  }
+
+  uploadPhoto(title: HTMLInputElement, description: HTMLInputElement){
+    this._uploadService.uploadImage(title.value, description.value, this.file).subscribe(data => console.log(data));
+  }
+
+  onPhotoSelected(event): void{
+    if(event.target.files && event.target.files[0]){
+      this.file = <File>event.target.files[0];
+      //Vista de la imagen
+      const reader = new FileReader();
+      reader.onload = e =>this.photoSelected = reader.result;
+      reader.readAsDataURL(this.file);
+    }
   }
 
   abrirArchivos() {
@@ -26,21 +50,21 @@ export class PuntoCincoEvidenciaPage implements OnInit {
     this.document.viewDocument('assets/CARTA RESPONSIVA CONTRATISTAS.pdf', 'aplication/pdf', options);
   }
 
-  bajarArchivo() {
-    let path = null;
+  // bajarArchivo() {
+  //   let path = null;
 
-    if (this.platform.is('ios')) {
-      path = this.file.documentsDirectory;
-    } else {
-      path = this.file.dataDirectory;
-    }
+  //   if (this.platform.is('ios')) {
+  //     path = this.file.documentsDirectory;
+  //   } else {
+  //     path = this.file.dataDirectory;
+  //   }
 
-    const transfer = this.transfer.create();
-    transfer.download('ruta', path + 'myfile.pdf').then(entry => {
-      let url = entry.toUrl();
-      this.document.viewDocument(url, 'application/pdf', {});
-    });
-  }
+  //   const transfer = this.transfer.create();
+  //   transfer.download('ruta', path + 'myfile.pdf').then(entry => {
+  //     let url = entry.toUrl();
+  //     this.document.viewDocument(url, 'application/pdf', {});
+  //   });
+  // }
 
   capturarFoto() {
     this.camera.getPicture({
--- src/app/pages/punto-siete-evidencia/punto-siete-evidencia.page.ts
+++ src/app/pages/punto-siete-evidencia/punto-siete-evidencia.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { EvidenciaElementoSieteService } from '../../services/Elemento 7/evidencia-elemento-siete.service';
 
 @Component({
   selector: 'app-punto-siete-evidencia',
@@ -6,10 +7,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./punto-siete-evidencia.page.scss'],
 })
 export class PuntoSieteEvidenciaPage implements OnInit {
+  photoSelected: String | ArrayBuffer;
+  file:File;
 
-  constructor() { }
+  constructor( private _uploadService : EvidenciaElementoSieteService) { }
 
   ngOnInit() {
+  }
+
+  uploadPhoto(title: HTMLInputElement, description: HTMLInputElement){
+    this._uploadService.uploadImage(title.value, description.value, this.file).subscribe(data => console.log(data));
+  }
+
+  onPhotoSelected(event): void{
+    if(event.target.files && event.target.files[0]){
+      this.file = <File>event.target.files[0];
+      //Vista de la imagen
+      const reader = new FileReader();
+      reader.onload = e =>this.photoSelected = reader.result;
+      reader.readAsDataURL(this.file);
+    }
   }
 
 }
